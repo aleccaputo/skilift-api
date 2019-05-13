91e877205c475ba9539c9d1ba057757a4acaa85c
Alec Caputo <acaputo@mail.smcvt.edu>
2019-05-13 19:57:50 -0400

    cors

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 const app = express();
 import {db} from "./config/db";
 import {routes} from "./app/routes";
@@ -8,6 +9,7 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const uri = db.url;
 mongoose.connect(uri, {useNewUrlParser: true});
